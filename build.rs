5b1d462f0ada5a4b149e05d4744bf482b22d2951
YOSHIDA Masanori <masanori.yoshida@gmail.com>
2020-02-21 12:06:27 +0900

    Modify build.rs to download pre-built libbls384_256.a instead of builiding on-the-fly

--- build.rs
+++ build.rs
@@ -3,38 +3,27 @@ use std::path::Path;
 use std::process::Command;
 use std::str::from_utf8;
 
+const LIB_REPO: &str = "bls-eth-go-binary";
+const LIB_DIR: &str = "bls/lib/linux/amd64";
+
 fn main() {
-    // cd $OUT_DIR
+    // get $OUT_DIR
     let out_dir = env::var("OUT_DIR").unwrap();
+
+    // RUSTFLAGS=-L$OUT_DIR/bls-eth-go-binary/bls/lib/linux/amd64
+    println!(
+        "cargo:rustc-link-search={}/{}/{}",
+        out_dir, LIB_REPO, LIB_DIR
+    );
+
+    // cd $OUT_DIR
     env::set_current_dir(&out_dir).unwrap();
 
-    println!("cargo:rustc-link-search={}/bls/lib", out_dir);
-
-    // git clone https://github.com/herumi/mcl
+    // git clone https://github.com/herumi/bls-eth-go-binary
     let ret = Command::new("git")
-        .args(&["clone", "https://github.com/herumi/mcl"])
+        .args(&["clone", &format!("https://github.com/herumi/{}", LIB_REPO)])
         .output()
         .unwrap();
-    if !ret.status.success() {
-        println!("{}", from_utf8(&ret.stderr).unwrap());
-        return;
-    }
-
-    // git clone https://github.com/herumi/bls
-    let ret = Command::new("git")
-        .args(&["clone", "https://github.com/herumi/bls"])
-        .output()
-        .unwrap();
-    if !ret.status.success() {
-        println!("{}", from_utf8(&ret.stderr).unwrap());
-        return;
-    }
-
-    // cd $OUT_DIR/bls
-    env::set_current_dir(Path::new(&out_dir).join("bls")).unwrap();
-
-    // make CXX=clang++
-    let ret = Command::new("make").arg("CXX=clang++").output().unwrap();
     if !ret.status.success() {
         println!("{}", from_utf8(&ret.stderr).unwrap());
         return;
